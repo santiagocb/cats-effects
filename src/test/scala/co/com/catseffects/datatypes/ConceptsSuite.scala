a257156c03e813760408024f4d89a6afa82de0b0
Santiago <santiagocadavidbte@gmail.com>
2021-01-06 21:56:38 -0500

    add: pure & async tests

--- src/test/scala/co/com/catseffects/datatypes/ConceptsSuite.scala
+++ src/test/scala/co/com/catseffects/datatypes/ConceptsSuite.scala
@@ -8,33 +8,55 @@ import scala.io.StdIn
 class ConceptsSuite extends FunSuite {
 
   test("Referential transparency") {
-
     val x = "Hello".reverse
     assert(x ++ x == "Hello".reverse ++ "Hello".reverse)
   }
 
   test("Broken Referential transparency") {
-
-    val x: Int = StdIn.readInt()
-
-    assert(x + x == StdIn.readInt() + StdIn.readInt())
+    val x: Int = {
+      println(s"Input a number: ")
+      StdIn.readInt()
+    }
+    assert(x + x == { println(s"Input a number: "); StdIn.readInt() } + {
+      println(s"Input a number: ")
+      StdIn.readInt()
+    })
   }
 
   test("Check if Future[_] has referential transparency property") {
-
     import scala.concurrent.ExecutionContext.Implicits.global
     import scala.concurrent.duration.Duration._
 
     //Same behaviour like above
-    val x: Future[Int] = Future(StdIn.readInt())
+    val x: Future[Int] = Future({
+      println(s"Input a number: ")
+      StdIn.readInt()
+    })
     val operation = for {
       a <- x
       b <- x
-      c <- Future(StdIn.readInt())
-      d <- Future(StdIn.readInt())
+      c <- Future({
+        println(s"Input a number: ")
+        StdIn.readInt()
+      })
+      d <- Future({
+        println(s"Input a number: ")
+        StdIn.readInt()
+      })
     } yield a + b == c + d
 
     assert(Await.result(operation, Inf))
+  }
+
+  test("By name & by value parameter") {
+    def byNameParameter(condition: => Boolean)(x: => Int) = if(condition) x else 0
+    def byValueParameter(condition: Boolean)(x: Int) = if(condition) x else 0
+
+    assert(byNameParameter(condition = false)(5) == 0)  //In this case de parameter x is not evaluated (this makes this parameter lazy)
+    assert(byNameParameter(condition = true)(2) == 2)   //x is evaluated
+    assert(byValueParameter(condition = false)(5) == 0)   //By value parameters are always evaluated but just once
+    assert(byValueParameter(condition = true)(2) == 2)   //x is evaluated once
+
   }
 
 
--- src/test/scala/co/com/catseffects/datatypes/IOSuite.scala
+++ src/test/scala/co/com/catseffects/datatypes/IOSuite.scala
@@ -3,7 +3,9 @@ package co.com.catseffects.datatypes
 import cats.effect.IO
 import org.scalatest.FunSuite
 
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.StdIn
+import scala.util.{Failure, Success, Try}
 
 class IOSuite extends FunSuite {
 
@@ -19,7 +21,7 @@ class IOSuite extends FunSuite {
 
   test("IO Monad") {
     val ioa = IO { println("hey!") }
-    println("This message must be printed before hey!")   //this is a side-effect, but it is there just for testing
+    println("This message must be printed before hey!")   // this is a side-effect, but it is there just for testing
 
     val program: IO[Unit] =
       for {
@@ -55,7 +57,6 @@ class IOSuite extends FunSuite {
   }
 
   test("IO stack safety support - fibonacci serie") {
-
     def fib(n: Int, a: Long = 0, b: Long = 1): IO[Long] = {
       IO(a + b).flatMap { b2 =>
         if (n > 0)
@@ -66,5 +67,34 @@ class IOSuite extends FunSuite {
     }
 
     assert(fib(5).unsafeRunSync() == 5)
+  }
+
+  test("Plus two values, one already into IO monad and other using pure function") {
+    val x = 2
+    val y = IO.pure(x)    //eager evaluated
+    val program = for {
+      a <- y
+      b <- IO(3)
+    } yield a + b
+
+    assert(program.unsafeRunSync() == 5)
+    assert(IO.unit == IO.pure(()))
+  }
+
+  test("Async function - convertion a future manually") {
+    def convert[A](fa: => Future[A])(implicit ec: ExecutionContext): IO[A] =
+      IO.async { cb =>
+        fa.onComplete {
+          case Success(a) => cb(Right(a))
+          case Failure(e) => cb(Left(e))
+        }
+      }
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    assert(convert(Future.successful(2)).unsafeRunSync() == 2)
+    assert(Try(convert(Future.failed(new RuntimeException("Runtime e"))).unsafeRunSync()).isFailure)
+    // Even if we wrap the onComplete method in IO Monad, we can have a runtime exception.
+    // In this case IO Monad help us to manage side effects that onComplete method carries.
+    // For that, we wrap the entire result in a Try.
   }
 }
